731769edce9ff163e4242a783865e18f84bee0b0
Charles G. Braga <charles.braga@solides.com.br>
2023-10-16 14:27:14 -0300

    adding points controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = 'spec/examples.txt'
   config.default_formatter = 'doc' if config.files_to_run.one?
-  config.profile_examples = 10
+  config.profile_examples = nil
   config.order = :random
   Kernel.srand config.seed
 
